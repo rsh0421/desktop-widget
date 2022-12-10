287f1bb78fb631b751a9bb1f966251da6f353014
DESKTOP-LAUTTLF\rsh04 <rsh0421@gmail.com>
2022-12-10 10:45:04 +0900

    Programming API.

--- test.js
+++ test.js
@@ -7,8 +7,10 @@ const WMI = wmi.connect('ROOT\\WMI');
 
 setInterval(()=>{
   let startTime = Date.now()
-  console.log(CIMV2.query('SELECT Name, PercentIdleTime, ProcessorFrequency FROM Win32_PerfFormattedData_Counters_ProcessorInformation'));
-  //CIMV2.query('SELECT Name, PercentIdleTime, ProcessorFrequency FROM Win32_PerfFormattedData_Counters_ProcessorInformation')
-  console.log(WMI.query('SELECT * FROM MSAcpi_ThermalZoneTemperature'));
+  //console.log(CIMV2.query('SELECT Name, NumberOfCores, NumberOfLogicalProcessors FROM Win32_Processor'));
+  //console.log(CIMV2.query('SELECT Name, PercentIdleTime, PercentProcessorTime, ProcessorFrequency FROM Win32_PerfFormattedData_Counters_ProcessorInformation'));
+  const obj = CIMV2.query('SELECT * FROM Win32_PerfFormattedData_PerfDisk_LogicalDisk WHERE Name="_Total"');
+  console.log(obj);
+  //console.log(WMI.query('SELECT * FROM MSAcpi_ThermalZoneTemperature'));
   console.log(Date.now()-startTime);
 }, 1000);
